5fc866346ab588338ece0ee7004c7e3a4f605e70
annmunyare <ann.munyare@nanyukiappfactory.onmicrosoft.com>
2018-11-06 11:47:02 +0300

    three icpak groups

--- resources/views/ICPAK/index.blade.php
+++ resources/views/ICPAK/index.blade.php
@@ -1,61 +1,46 @@
-
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-   
-    <title>Data Catalog</title>
-  
-  </head>
-  <body> 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-   <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-   <span class="navbar-toggler-icon"></span>
-   </button>
-   <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-   
-         <li class="nav-item">
-            <a class="nav-link" href="/groups">ICPAK</a>
-         </li>
-         
-      </ul>
-   </div>
-</nav><div class="container">
-
-<p class="font-weight-bold "  >ICPAK Discussions</p>
-   <table class="table table-condensed table-striped table-bordered table-hover  ">
-   <tr>
-   
-   <th> Discussion</th>
- 
-   <th colspan = '1'>Actions</th>
-   </tr>
-   @foreach ($obj as $group)
-
-
-   <tr>
- 
-   <td>{{$group['groupName']}} </td>
-
-   <td>
-   <a href="https://join.kaiza.la/p/iLsRcimZQweiJ_wtblQbng" class="btn btn-sm btn-info">Join</a>
-   </td>
-   </tr>
-@endforeach
-</table>
- 
-</div>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
+   <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <title>ICPAK Discussions</title>
+   </head>
+   <body>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+               <li class="nav-item">
+                  <a class="nav-link" href="/groups">ICPAK</a>
+               </li>
+            </ul>
+         </div>
+      </nav>
+      <div class="container">
+         <p class="font-weight-bold "  >ICPAK Discussions</p>
+         <table class="table table-condensed table-striped table-bordered table-hover  ">
+            <tr>
+               <th> Discussion</th>
+            </tr>
+            <tr>
+               <td><a href="https://join.kaiza.la/p/CiD1f3PYT6mWsg_w_akAnw" >Financial Reporting</a> </td>
+            </tr>
+            <tr>
+               <td><a href="https://join.kaiza.la/p/u_Svfed1T0W2hoDUKwC-cQ" >Professional Ethics</a> </td>
+            </tr>
+            <tr>
+               <td><a href="https://join.kaiza.la/p/m4csERi1RPy0Q7sgbx0ikA" >Auditing</a> </td>
+            </tr>
+         </table>
+      </div>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+   </body>
 </html>
-
