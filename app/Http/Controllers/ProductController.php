721765f246d9b4825ec6165cd525b5a2bc812720
annmunyare <ann.munyare@nanyukiappfactory.onmicrosoft.com>
2018-11-06 15:28:15 +0300

    fixed error

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -34,30 +34,30 @@ class ProductController extends Controller
       
         // 'https://kms.kaiza.la/v1/groups/0b9e0148-1006-47c5-b371-5bc80e1aa098/subGroups'
 
-        $client = new Client();
-        $response = $client->request('GET', 'https://kms.kaiza.la/v1/groups/', [
-            'headers' => [
-            'Accept' => 'application/json',
-            'Content-type' => 'application/json',
-            'accessToken' =>'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cm46bWljcm9zb2Z0OmNyZWRlbnRpYWxzIjoie1wicGhvbmVOdW1iZXJcIjpcIisyNTQ3MTM2MjQyNTRcIixcImNJZFwiOlwiXCIsXCJ0ZXN0U2VuZGVyXCI6XCJmYWxzZVwiLFwiYXBwTmFtZVwiOlwiY29tLm1pY3Jvc29mdC5tb2JpbGUua2FpemFsYWFwaVwiLFwiYXBwbGljYXRpb25JZFwiOlwiZmE3ZGJmOWUtMTA4YS00ZWQwLTliNzYtM2M0NTg5NzU0NDY0XCIsXCJwZXJtaXNzaW9uc1wiOlwiMi4zMDozLjMwOjQuMjo2LjIyOjUuNDo5LjI6MTUuMzA6MTQuMzA6MTkuMzA6MjQuMzBcIixcImFwcGxpY2F0aW9uVHlwZVwiOjMsXCJkYXRhXCI6XCJ7XFxcIkFwcE5hbWVcXFwiOlxcXCJhbm5Db25uZWN0b3IyXFxcIn1cIn0iLCJ1aWQiOiJNb2JpbGVBcHBzU2VydmljZToyNDVhYjIzZC0xZGMwLTQyZGYtYmE2ZC1kYzYwMTZlZGY4NjciLCJ2ZXIiOiIyIiwibmJmIjoxNTQxNDEzOTgxLCJleHAiOjE1NDE1MDAzODEsImlhdCI6MTU0MTQxMzk4MSwiaXNzIjoidXJuOm1pY3Jvc29mdDp3aW5kb3dzLWF6dXJlOnp1bW8iLCJhdWQiOiJ1cm46bWljcm9zb2Z0OndpbmRvd3MtYXp1cmU6enVtbyJ9.RNh1U-pBs8zOV-TeTscTHjhfT7GZq7Nrs085irCFVuY'
-        ]]);
-        $body = $response->getBody();
-        $content =$body->getContents();
-        $arr = json_decode($content,TRUE);
-    //    return $content;
-    // return $arr['groupName'];
-        foreach($arr as $obj )
-        {
+    //     $client = new Client();
+    //     $response = $client->request('GET', 'https://kms.kaiza.la/v1/groups/', [
+    //         'headers' => [
+    //         'Accept' => 'application/json',
+    //         'Content-type' => 'application/json',
+    //         'accessToken' =>'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cm46bWljcm9zb2Z0OmNyZWRlbnRpYWxzIjoie1wicGhvbmVOdW1iZXJcIjpcIisyNTQ3MTM2MjQyNTRcIixcImNJZFwiOlwiXCIsXCJ0ZXN0U2VuZGVyXCI6XCJmYWxzZVwiLFwiYXBwTmFtZVwiOlwiY29tLm1pY3Jvc29mdC5tb2JpbGUua2FpemFsYWFwaVwiLFwiYXBwbGljYXRpb25JZFwiOlwiZmE3ZGJmOWUtMTA4YS00ZWQwLTliNzYtM2M0NTg5NzU0NDY0XCIsXCJwZXJtaXNzaW9uc1wiOlwiMi4zMDozLjMwOjQuMjo2LjIyOjUuNDo5LjI6MTUuMzA6MTQuMzA6MTkuMzA6MjQuMzBcIixcImFwcGxpY2F0aW9uVHlwZVwiOjMsXCJkYXRhXCI6XCJ7XFxcIkFwcE5hbWVcXFwiOlxcXCJhbm5Db25uZWN0b3IyXFxcIn1cIn0iLCJ1aWQiOiJNb2JpbGVBcHBzU2VydmljZToyNDVhYjIzZC0xZGMwLTQyZGYtYmE2ZC1kYzYwMTZlZGY4NjciLCJ2ZXIiOiIyIiwibmJmIjoxNTQxNDEzOTgxLCJleHAiOjE1NDE1MDAzODEsImlhdCI6MTU0MTQxMzk4MSwiaXNzIjoidXJuOm1pY3Jvc29mdDp3aW5kb3dzLWF6dXJlOnp1bW8iLCJhdWQiOiJ1cm46bWljcm9zb2Z0OndpbmRvd3MtYXp1cmU6enVtbyJ9.RNh1U-pBs8zOV-TeTscTHjhfT7GZq7Nrs085irCFVuY'
+    //     ]]);
+    //     $body = $response->getBody();
+    //     $content =$body->getContents();
+    //     $arr = json_decode($content,TRUE);
+    // //    return $content;
+    // // return $arr['groupName'];
+    //     foreach($arr as $obj )
+    //     {
             
-            // return $obj;
+    //         // return $obj;
             
          
-         }
+    //      }
      
 
        
      
-        return view('ICPAK.index', compact('obj'));
+        return view('ICPAK.index');
     }
 
     public function index()
